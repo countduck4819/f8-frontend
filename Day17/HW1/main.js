8ee69c0456ba105e8e4e1c27a1efe7de8838ea75
khang2003 <17loiten@gmail.com>
2023-08-15 08:23:18 +0700

    Hoàn thành bài Day17

--- Day17/HW1/main.js
+++ Day17/HW1/main.js
@@ -1,26 +1,41 @@
-// var lorem = "Lorem ipsum dolor sit"
-// lorem += " ";
-// var acc = function (lorem) {
-//     var index = 0;
-//     var chuoi = lorem;
-//     for (var i = 0; i < lorem.length; i++) {
-//         if (lorem.charAt(i) === " ") {
-//             let select = lorem.slice(index, i);
-//             chuoi = lorem.slice(0,index)+"<span>"+select+ "</span> " + lorem.slice(i + 1);
-//             index = i + 1;
-//             document.body.innerHTML = chuoi 
-//         }    
+var lorem = "Lorem ipsum dolor sit"
+lorem += " ";
+function th(a) {
+    return function (b) {
+        a = a + 500;
+        setTimeout(function() {
+            document.body.innerHTML = b;
+        },a)
+    }
+}
+var c = th(500);
+var acc = function (lorem) {
+    var index = 0;
+    var chuoi = lorem;
+    for (var i = 0; i < lorem.length; i++) {
+        if (lorem.charAt(i) === " ") {
+            let select = lorem.slice(index, i);
+            chuoi = lorem.slice(0,index)+"<span>"+select+ "</span> " + lorem.slice(i + 1);
+            index = i + 1;
+            c(chuoi)
+        }    
+    }
+}
+let dem = 0;
+while(dem < 10) {
+    acc(lorem);
+    dem++;
+}
+
+// function th(a) {
+//     return funciton (b) {
+//         a = a + a;
+//         setTimeout(function() {
+
+//         },a)
 //     }
 // }
-
-// setTimeout(function() {
-
-// },1000)
-
-
-// var a = "123";
-// console.log(a.slice(0,0))
-
+// document.body.innerHTML = chuoi 
 
 // function b(a) {
 //     return function (b) {
@@ -31,10 +46,9 @@
 //     }
 // }
 // var c = b(500);
-// c(10);
-// c(12)
-
-
+// for(var i = 0; i < 2; i++) {
+//     c(i);
+// }
 
 
 
