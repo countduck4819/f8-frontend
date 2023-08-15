f80d699dc31ced56155fe0a79ae059e7e075ece0
khang2003 <17loiten@gmail.com>
2023-08-15 07:59:43 +0700

    Hoàn thành bài tập Day17

---
+++ Day17/HW1/main.js
@@ -0,0 +1,41 @@
+// var lorem = "Lorem ipsum dolor sit"
+// lorem += " ";
+// var acc = function (lorem) {
+//     var index = 0;
+//     var chuoi = lorem;
+//     for (var i = 0; i < lorem.length; i++) {
+//         if (lorem.charAt(i) === " ") {
+//             let select = lorem.slice(index, i);
+//             chuoi = lorem.slice(0,index)+"<span>"+select+ "</span> " + lorem.slice(i + 1);
+//             index = i + 1;
+//             document.body.innerHTML = chuoi 
+//         }    
+//     }
+// }
+
+// setTimeout(function() {
+
+// },1000)
+
+
+// var a = "123";
+// console.log(a.slice(0,0))
+
+
+// function b(a) {
+//     return function (b) {
+//         a = a + a;
+//         setTimeout(function(){
+//             document.write(b);
+//         },a)
+//     }
+// }
+// var c = b(500);
+// c(10);
+// c(12)
+
+
+
+
+
+
