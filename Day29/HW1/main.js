5701e997c3cc799973beacdf0a996c7d154e692c
khang2003 <17loiten@gmail.com>
2023-09-29 20:26:49 +0700

    Hoàn thành bài tập Day29

--- Day29/HW1/main.js
+++ Day29/HW1/main.js
@@ -9,92 +9,138 @@ var stringHTML = "";
 var stt = 1;
 var stringTotal = `<tr class="total">
 <td colspan="3">Tổng</td>
-<td>2</td>
+<td><input type="text"/></td>
 <td colspan="2">300</td>
 </tr>`;
 listQuantity.length = subItem.length - 1;
 listQuantity.fill(0);
 var quantityProduct = 0;
-var updatePrice = function() {
-    Array.from(tableShop.children).forEach(function(value){
+var updatePrice = function () {
+    Array.from(tableShop.children).forEach(function (value) {
         if (+value.getAttribute("data-index")) {
             var _index = +value.dataset.index;
-            var price = subItem[_index].querySelector(".price").innerText;
+            value.querySelector(".total-item").innerText = listQuantity[_index - 1];
         }
-    })
-}
-var updateTotal = function() {
+    });
+};
+var updateTotal = function () {
     var total = 0;
-    Array.from(tableShop.children).forEach(function(value){
+    var count = 0;
+    Array.from(tableShop.children).forEach(function (value) {
         if (+value.getAttribute("data-index")) {
             var _index = +value.dataset.index;
             total += listQuantity[_index - 1];
             var price = subItem[_index].querySelector(".price").innerText;
-            value.querySelector("[type='number']").value = listQuantity[_index - 1] /  price
+            count += +listQuantity[_index - 1] / price;
+            value.querySelector("[type='number']").value = listQuantity[_index - 1] / price;
         }
-    })
-    tableShop.querySelector(".total").lastElementChild.innerText = Number(total).toLocaleString("vi",{
-        style: "currency",
-        currency: "VND"
     });
-}
+    tableShop.querySelector(".total").lastElementChild.previousElementSibling.children[0].value = count;
+    tableShop.querySelector(".total").lastElementChild.innerText = Number(
+        total
+    ).toLocaleString("vi", {
+        style: "currency",
+        currency: "VND",
+    });
+};
 
-var removeElement = function(){
-    
-}
+var removeElement = function (rev, value) {
+    rev.addEventListener("click", function (e) {
+        var _index = value.getAttribute("data-index")
+        listQuantity[_index - 1] = 0;
+        quantityProduct--;
+        updatePrice();
+        value.remove();
+        if (quantityProduct === 0) {
+            cart.innerHTML = `<p>Giỏ hàng không có sản phầm nào</p>`
+            stringHTML = "";
+            stt = 1;
+        }
+        else {
+            stt = 1;
+            stringHTML = `<tr><th width="5.3%">STT</th><th>Tên sản phẩm</th><th width="20%">Giá</th><th width="20%">Số lượng</th><th width="20%">Thành tiền</th><th width="5.3%">Xoá</th></tr>`;
+            Array.from(tableShop.children).forEach(function(value,id){
+                var price = subItem[id].querySelector(".price")
+                console.log(price)
+                var actionItem = subItem[_index].querySelector(".action").children[0];
+                if (value.getAttribute("data-index") && quantityProduct[value.getAttribute("data-index") - 1] !== 0) {
+                    console.log(value)
+                    stringHTML += `<tr data-index="${value.dataset.index}">
+                    <td>${stt}</td>
+                    <td>${price.previousElementSibling.innerText}</td>
+                    <td>${price.innerText}</td>
+                    <td><input type="number" min="1" name="" id="" value="${
+                        actionItem.value
+                    }"></td>
+                    <td class="total-item">${listQuantity[_index - 1]}</td>
+                    <td><button class="remove" type="button">
+                        Xóa
+                    </button></td>
+                </tr>`;
+                tableShop.innerHTML = stringHTML + stringTotal;
+                stt++;
+                } 
+            })
+            updateTotal();
+        }
+    });
+};
 
 Array.from(subItem).forEach(function (value, index) {
     var actionItem = value.querySelector(".action");
     if (index !== 0) {
-        var price = value.querySelector(".price")
+        var price = value.querySelector(".price");
         var inputSubItem = actionItem.children[0];
         var buttonSubItem = actionItem.children[1];
         buttonSubItem.addEventListener("click", function (e) {
-            console.log(index)
             if (quantityProduct === 0) {
                 var table = `<table border="1" width="100%" cellspacing="0px"cellpadding="0px"><tbody></tbody></table><hr/><button>Cập nhật giỏ hàng</button><button>Xóa giỏ hàng</button>`;
                 cart.innerHTML = table;
                 stringHTML += `<tr><th width="5.3%">STT</th><th>Tên sản phẩm</th><th width="20%">Giá</th><th width="20%">Số lượng</th><th width="20%">Thành tiền</th><th width="5.3%">Xoá</th></tr>`;
                 tableShop = cart.getElementsByTagName("table")[0].children[0];
             }
-            console.log(listQuantity)
             if (listQuantity[index - 1] === 0) {
                 quantityProduct++;
-                listQuantity[index - 1] += (price.innerText * inputSubItem.value);
-    
+                listQuantity[index - 1] += price.innerText * inputSubItem.value;
+
                 stringHTML += `<tr data-index="${index}">
                 <td>${stt}</td>
                 <td>${price.previousElementSibling.innerText}</td>
                 <td>${price.innerText}</td>
-                <td><input type="number" min="1" name="" id="" value="${inputSubItem.value}"></td>
+                <td><input type="number" min="1" name="" id="" value="${
+                    inputSubItem.value
+                }"></td>
                 <td class="total-item">${listQuantity[index - 1]}</td>
                 <td><button class="remove" type="button">
                     Xóa
                 </button></td>
-            </tr>`
-                tableShop.innerHTML = stringHTML + stringTotal
-                tableShop.querySelector(".total");
+            </tr>`;
+                tableShop.innerHTML = stringHTML + stringTotal;
                 updateTotal();
                 stt++;
                 updatePrice();
-            }
-            else {
-                Array.from(tableShop.children).forEach(function(value,_index){
-                    console.log(123,value.getAttribute("data-index") === index)
-                    console.log(typeof value.getAttribute("data-index"),typeof index)
-                    if (+value.getAttribute("data-index") === index) {
-                        listQuantity[index - 1] += +(price.innerText * inputSubItem.value);
-                        value.querySelector(".total-item").innerText = listQuantity[index - 1]
-                        console.log(listQuantity)
+                Array.from(tableShop.children).forEach(function(value){
+                    if (value.getAttribute("data-index")) {
+                        var _index = +value.getAttribute("data-index")
+                        removeElement(value.querySelector(`.remove`),value);
                     }
                 })
+    
+            } else {
+                Array.from(tableShop.children).forEach(function (
+                    value,
+                    _index
+                ) {
+                    if (+value.getAttribute("data-index") === index) {
+                        listQuantity[index - 1] += +(
+                            price.innerText * inputSubItem.value
+                        );
+                        value.querySelector(".total-item").innerText =
+                            listQuantity[index - 1];
+                    }
+                });
                 updateTotal();
             }
-            Array.from(tableShop.children).forEach(function(value,_index){
-                if (value.getAttribute("data-index")) {
-
-                }
-            })
         });
     }
 });
