131429d429ae0d55e7d1c1461797e96f477a23ce
khang2003 <17loiten@gmail.com>
2023-10-12 17:11:20 +0700

    Hoàn thành bài tập Day33

--- Day32/HW1/main.js
+++ Day32/HW1/main.js
@@ -1,7 +1,7 @@
-var container = document.querySelector(".container")
+var container = document.querySelector(".container");
 var button = document.querySelector(".container button");
-var countDown = document.createElement("div")
-var textNode = document.createTextNode("30")
+var countDown = document.createElement("div");
+var textNode = document.createTextNode("30");
 var currentDataTextNode = textNode.data;
 var updateCount = 0;
 var timeCount;
@@ -11,33 +11,29 @@ var check = 0;
 var visibilityState = "visible";
 var animationFrame;
 countDown.classList.add("countdown");
-container.insertBefore(countDown,button)
+container.insertBefore(countDown, button);
 countDown.append(textNode);
 var vis = new Event("visible");
 var hid = new Event("hidden");
 var desCount = function () {
-    
     window.addEventListener("visibilitychange", () => {
         if (window.visibilityState === "hidden") {
             visibilityState = "hidden";
-            console.log(visibilityState);
+        } else {
+            visibilityState = "visible";
         }
-        else {
-                    visibilityState = "visible";
-                }
     });
-    // console.log(document.visibilityState, document.visibilityState === "hidden")
+    console.log(document.visibilityState)
     if (visibilityState === "hidden") {
         // console.log("di choi")
-        startTime = performance.now()
+        startTime = performance.now();
         check = 1;
         currentDataTextNode = +textNode.data;
-    }
-    else {
+    } else {
         // console.log(startTime,endTime,check)
         if (check === 1) {
             startTime = performance.now();
-            check = 0
+            check = 0;
         }
         endTime = performance.now();
         timeCount = endTime - startTime;
@@ -51,30 +47,32 @@ var desCount = function () {
     animationFrame = requestAnimationFrame(desCount);
     // // console.log(+textNode.data === 0,+textNode.data)
     if (+textNode.data === 0 || timeCount >= +currentDataTextNode) {
-        button.dispatchEvent(vis)
+        button.dispatchEvent(vis);
         textNode.data = 0;
         cancelAnimationFrame(animationFrame);
     }
     updateCount = timeCount;
-}
-desCount()
-button.addEventListener("visible",function() {
-    this.addEventListener("click", function(e) {
-        var urlEncode = unescape('https%3A//www.youtube.com/watch%3Fv%3DZ-aoaJvegHE%26ab_channel%3DK%25E1%25BA%25BB%25C4%2590%25C3%25A1ngTh%25C6%25B0%25C6%25A1ng')
-        window.location.href = urlEncode
-    })
-    
+};
+desCount();
+button.addEventListener("visible", function () {
+    this.addEventListener("click", function (e) {
+        var urlEncode = unescape(
+            "https%3A//www.youtube.com/watch%3Fv%3DZ-aoaJvegHE%26ab_channel%3DK%25E1%25BA%25BB%25C4%2590%25C3%25A1ngTh%25C6%25B0%25C6%25A1ng"
+        );
+        window.location.href = urlEncode;
+    });
+
     this.classList.remove("hidden");
     this.classList.add("visible");
-})
+});
 
-button.addEventListener("hidden",function(e) {
+button.addEventListener("hidden", function (e) {
     this.classList.remove("visible");
     this.classList.add("hidden");
-})
+});
 
 if (+textNode.innerText === 0) {
-    button.dispatchEvent(vis)
+    button.dispatchEvent(vis);
 }
 
 // // window.addEventListener("visibilitychange", function() {
@@ -84,7 +82,6 @@ if (+textNode.innerText === 0) {
 // //         console.log("Browser tab is hiddend")
 // //     }
 // // });
-
 
 // document.addEventListener("visibilitychange", () => {
 //     console.log(document.visibilityState);
