2b8c58aa3113b68997de73deccb3d6055a4406ac
khang <17loiten@gmail.com>
2023-12-11 17:16:35 +0700

    Hoàn thành bài tập Day50

--- Day50/HW1/homework-day50/src/App.jsx
+++ Day50/HW1/homework-day50/src/App.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import LoginButton from './components/LoginButton'
 import Profile from './components/Profile'
 import { GlobalContext } from './reducers/Provider'
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
   const {state,dispatch} = useContext(GlobalContext);
+  const { user, isAuthenticated, isLoading } = useAuth0();
   return (
     <>
-      {state.checkLogin ? <Profile/> : <div className="container">
+      {state.checkLogin && isAuthenticated ? <Profile/> : <div className="container">
       <div>Cảm ơn bạn đã sử dụng dịch vụ của F8</div>
       <div className="desc">Nếu có bất kỳ câu hỏi hay trợ giúp nào, hãy đăng nhập và đặt câu hỏi tại đây</div>
       <LoginButton/>
--- Day50/HW1/homework-day50/src/components/LoginButton.jsx
+++ Day50/HW1/homework-day50/src/components/LoginButton.jsx
@@ -3,10 +3,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { GlobalContext } from "../reducers/Provider";
 
 const LoginButton = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
   const { loginWithPopup } = useAuth0();
   const {state,dispatch} = useContext(GlobalContext);
-  return (state.checkLogin && isAuthenticated) || <button className="btn" onClick={() => {
+  return <button className="btn" onClick={() => {
     dispatch({
         type: "check/login",
         payload: true})
